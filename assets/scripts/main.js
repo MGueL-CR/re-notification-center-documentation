d9452b281170fc72b427719409dfc3a39ee9dda8
MGueL-CR <mi.migue437@gmail.com>
2026-08-23 01:51:35 -0600

    Cambios en comportamiento y diseño

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menu = document.getElementById("menu-main");
     const contenedor = document.getElementById("show-content");
+    const todo = document.getElementById("main-content");
 
     menu.addEventListener("click", async (evento) => {
         if (evento.target.classList.contains("list-item")) {
-
             const pagina = evento.target.getAttribute("data-name");
             const urlDestino = pagina
                 ? `/assets/pages/${pagina}.html`
@@ -19,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 const html = await respuesta.text();
                 contenedor.innerHTML = html;
+                todo.scrollIntoView({ behavior: "smooth" });
             } catch (error) {
                 contenedor.innerHTML = `<div class="card warn"> <h2>Error al cargar la sección! </h2> <hr /><p>Detalle:</p><p> ${error.message}</p></div>`;
             }
