63fd73d9c164a9a4211a3d2a70eafdfad7ac46d3
MGueL-CR <mi.migue437@gmail.com>
2026-08-25 01:25:21 -0600

    Incorporando menu superior y funcionalidad P1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,28 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const menu = document.getElementById("menu-main");
+    const menuMain = document.getElementById("menu-main");
     const contenido = document.getElementById("show-content");
     const contenedor = document.getElementById("main-content");
+    const headerPage = document.getElementById('headerPage');
+    const menuTop = document.getElementById("menuTop");
 
-    menu.addEventListener("click", async (evento) => {
-        if (evento.target.classList.contains("list-item")) {
-            const pagina = evento.target.getAttribute("data-name");
-            const urlDestino = pagina
-                ? `/assets/pages/${pagina}.html`
-                : "/assets/pages/pag404.html";
-
-            try {
-                const respuesta = await fetch(urlDestino);
-
-                if (!respuesta.ok) {
-                    throw new Error("No se pudo cargar o mostrar el contenido");
-                }
-
-                const html = await respuesta.text();
-                contenido.innerHTML = html;
-                contenedor.scrollIntoView({ behavior: "smooth" });
-            } catch (error) {
-                contenido.innerHTML = `<div class="card warn"> <h2>Error al cargar la sección! </h2> <hr /><p>Detalle:</p><p> ${error.message}</p></div>`;
-            }
+    menuMain.addEventListener("click", (evento) => {
+        const elemento = evento.target;
+        if (elemento.classList.contains("list-item")) {
+            const pagina = elemento.getAttribute("data-name");
+            loadContentPage(pagina, contenido, contenedor);
         }
     });
+
+    menuTop.addEventListener("click", (evento) => {
+        const elemento = evento.target;
+        if (elemento.classList.contains("menu-item")) {
+            const pagina = elemento.getAttribute("data-name");
+            loadContentPage(pagina, contenido, headerPage);
+        }
+
+        if (elemento.classList.contains("submenu-item")) {
+            const pagina = elemento.getAttribute("data-name");
+            loadContentPage(pagina, contenido, contenedor);
+        }
+    });
+
+    loadContentPage("intro-main", contenido, headerPage);
 });
+
+async function loadContentPage(pagina, contenido, contenedor) {
+
+
+    const urlDestino = pagina
+        ? `/assets/pages/${pagina}.html`
+        : "/assets/pages/pag404.html";
+
+    try {
+        const respuesta = await fetch(urlDestino);
+
+        if (!respuesta.ok) {
+            //throw new Error("No se pudo cargar o mostrar el contenido");
+            console.error("Error 404!", "No se pudo cargar o mostrar el contenido. El archivo o página no se encuentró.");
+            loadContentPage("pag404", contenido, contenedor);
+            return;
+        }
+
+        const html = await respuesta.text();
+        contenido.innerHTML = html;
+        contenedor.scrollIntoView({ behavior: "smooth" });
+    } catch (error) {
+        contenido.innerHTML = `<div class="card warn"> <h2>Error al cargar la sección! </h2> <hr /><p>Detalle:</p><p> ${error.message}</p></div>`;
+    }
+
+}
