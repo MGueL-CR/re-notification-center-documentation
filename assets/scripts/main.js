c551fecf044ecc80e9397454df274d63a88e5d77
mguel.dev <mguel.dev@proton.me>
2026-08-27 15:37:42 -0600

    Incorporando menu superior y funcionalidad P2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,28 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const menuMain = document.getElementById("menu-main");
-    const contenido = document.getElementById("show-content");
     const contenedor = document.getElementById("mainContent");
     const headerPage = document.getElementById('headerPage');
     const menuTop = document.getElementById("menuTop");
-    /*
-        menuMain.addEventListener("click", (evento) => {
-            const elemento = evento.target;
-            if (elemento.classList.contains("list-item")) {
-                const pagina = elemento.getAttribute("data-name");
-                loadContentPage(pagina, contenido, contenedor);
-            }
-        });*/
 
     menuTop.addEventListener("click", (evento) => {
         const elemento = evento.target;
         if (elemento.classList.contains("menu-item")) {
-            const pagina = elemento.getAttribute("data-name");
-            loadContentPage(pagina, contenedor, headerPage);
+            const pagina = elemento.dataset.name;
+            loadContentPage(pagina, contenedor, headerPage).then(() => {
+                if (elemento.dataset.section) {
+                    activateLinksSideMenu();
+                }
+            });
         }
 
         if (elemento.classList.contains("submenu-item")) {
-            const pagina = elemento.getAttribute("data-name");
-            loadContentPage(pagina, contenido, contenedor);
+            const pagina = elemento.dataset.name;
+            const superior = elemento.closest('.menu-item');
+            if (superior.dataset.section) {
+                loadContentPage(`${superior.dataset.section}-folder`, contenedor, headerPage).then(() => {
+                    const contenido = document.getElementById("show-content");
+                    activateLinksSideMenu();
+                    loadContentPage(pagina, contenido, contenedor);
+                });
+            } else {
+                loadContentPage(pagina, contenedor, headerPage);
+            }
         }
     });
 
@@ -52,5 +55,17 @@ async function loadContentPage(pagina, contenido, contenedor) {
     } catch (error) {
         contenido.innerHTML = `<div class="card warn"> <h2>Error al cargar la sección! </h2> <hr /><p>Detalle:</p><p> ${error.message}</p></div>`;
     }
+}
 
+function activateLinksSideMenu() {
+    const menuMain = document.getElementById("menu-main");
+    const contenedor = document.getElementById("mainContent");
+    const contenido = document.getElementById("show-content");
+    menuMain.addEventListener("click", (evento) => {
+        const elemento = evento.target;
+        if (elemento.classList.contains("list-item")) {
+            const pagina = elemento.dataset.name;
+            loadContentPage(pagina, contenido, contenedor);
+        }
+    });
 }
